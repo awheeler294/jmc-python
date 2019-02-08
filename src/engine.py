4cd0d188103aa8db405f746c90a8c6e9cea721c4
awheeler294 <awheeler294@gmail.com>
2019-02-07 23:02:24 -0700

    display character

--- src/engine.py
+++ src/engine.py
@@ -1,10 +1,25 @@
-import libtcodpy as libtcod
+from pathlib import Path
 
+import tcod as libtcodpy
+
+from presenter import Presenter
 
 def main():
-    screen_width = 80
-    screen_height = 50
+    window_width = 80
+    window_height = 50
+    window_title = "Janus Mining Colony"
+    font_path = Path("fonts/libtcod/dejavu16x16_gs_tc.png")
 
+    presenter = Presenter(window_width, window_height, window_title, font_path)
+
+    while presenter.is_window_open():
+        presenter.put_tile(1, 1, '@', libtcodpy.white)
+        presenter.present()
+
+        key = libtcodpy.console_check_for_keypress()
+
+        if key.vk == libtcodpy.KEY_ESCAPE:
+            return True
 
 if __name__ == '__main__':
     main()
--- src/presenter.py
+++ src/presenter.py
@@ -1,13 +1,20 @@
-import libtcodpy as libtcod
+import tcod as libtcodpy
 
 class Presenter:
     def __init__(self, window_width, window_height, window_title, font_path):
         self._window_width = window_width
         self._window_height = window_height
 
-        libtcod.console_set_custom_font(str(font_path), libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
+        libtcodpy.console_set_custom_font(str(font_path), libtcodpy.FONT_TYPE_GRAYSCALE | libtcodpy.FONT_LAYOUT_TCOD)
 
-        libtcod.console_init_root(window_width, window_height, window_title, False)
+        libtcodpy.console_init_root(window_width, window_height, window_title, False)
 
     def is_window_open(self):
-        return not libtcod.console_is_window_closed()
+        return not libtcodpy.console_is_window_closed()
+
+    def put_tile(self, x, y, symbol, color):
+        libtcodpy.console_set_default_foreground(0, color)
+        libtcodpy.console_put_char(0, x, y, symbol)
+
+    def present(self):
+        libtcodpy.console_flush()
